d32b59198c4e4c09a7610c5622e5ec15444f471f
Antonio César <antoniocesarfernandes@gmail.com>
2023-06-23 15:16:18 -0300

    Termino do projetinho

--- src/services/CarroService.js
+++ src/services/CarroService.js
@@ -9,5 +9,68 @@ module.exports = {
                 aceito(results);
             })
         })
+    },
+
+    buscarUm:(codigo)=>{
+        return new Promise((aceito,rejeitado)=>{
+            db.query('SELECT * FROM carros WHERE codigo = ?',[codigo],(error, results)=>{
+                if(error){
+                    rejeitado(error);
+                    return;
+                }
+                if(results.length>0){
+                    aceito(results[0]);
+                }else{
+                    aceito(false);
+                }
+            })
+        })
+    },
+
+    inserir:(modelo,placa)=>{
+        return new Promise((aceito,rejeitado)=>{
+            db.query('INSERT INTO carros (modelo,placa) VALUES (?,?)',
+                [modelo,placa],
+                (error, results)=>{
+                    if(error){
+                        rejeitado(error);
+                        return;
+                    }
+                    aceito(results.insertCodigo);
+                }
+            );
+        });
+    },
+
+    alterar:(codigo, modelo,placa)=>{
+        return new Promise((aceito,rejeitado)=>{
+            db.query('UPDATE carros SET modelo = ?, placa = ? WHERE codigo = ?',
+                [modelo,placa,codigo],
+                (error, results)=>{
+                    if(error){
+                        rejeitado(error);
+                        return;
+                    }
+                    aceito(results);
+                }
+            );
+        });
+    },
+
+    excluir: (codigo) =>{
+        return new Promise((aceito,rejeitado)=>{
+
+            db.query('DELETE FROM carros WHERE codigo = ?',
+            [codigo],
+            (error, results)=>{
+                if(error){ 
+                    rejeitado(error); 
+                    return;
+                }
+                aceito(results);
+            });
+        });
     }
-};
+
+
+};//fim do módulo exports
